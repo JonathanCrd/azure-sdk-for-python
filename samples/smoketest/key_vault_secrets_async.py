80a5a551b88302f18d44a554e4c51560f417e373
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2019-07-29 12:14:04 -0700

    Document Formatting

--- samples/smoketest/key_vault_secrets_async.py
+++ samples/smoketest/key_vault_secrets_async.py
@@ -2,6 +2,7 @@ import os
 from azure.identity.aio import DefaultAzureCredential
 from azure.keyvault.secrets.aio import SecretClient
 
+
 class KeyVault_async:
     def __init__(self):
         # DefaultAzureCredential() expects the following environment variables:
@@ -9,13 +10,17 @@ class KeyVault_async:
         # * AZURE_CLIENT_SECRET
         # * AZURE_TENANT_ID
         credential = DefaultAzureCredential()
-        self.secret_client = SecretClient(vault_url=os.environ["AZURE_PROJECT_URL"], credential=credential)
+        self.secret_client = SecretClient(
+            vault_url=os.environ["AZURE_PROJECT_URL"], credential=credential
+        )
         self.secret_name = "MySecret"
         self.secret_value = "My Secret Value"
 
     async def SetSecret(self):
         print("Setting a secret...")
-        secret = await self.secret_client.set_secret(self.secret_name, self.secret_value)
+        secret = await self.secret_client.set_secret(
+            self.secret_name, self.secret_value
+        )
         print("\tdone, secret: (" + secret.name + "," + secret.value + ").")
 
     async def GetSecret(self):
@@ -25,7 +30,7 @@ class KeyVault_async:
 
     async def DeleteSecret(self):
         print("Deleting a secret...")
-        deleted_secret = await self.secret_client.delete_secret(self.secret_name)
+        await self.secret_client.delete_secret(self.secret_name)
         print("\tdone")
 
     async def Run(self):
